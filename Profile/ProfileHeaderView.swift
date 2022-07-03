ea8c509a32af9b23e74e0d0bcaa1e45cd99536c7
Roman Bespalov <voordex@gmail.com>
2022-07-03 21:46:16 +0300

    HW 2.1 done

--- Profile/ProfileHeaderView.swift
+++ Profile/ProfileHeaderView.swift
@@ -9,31 +9,69 @@ import UIKit
 
 class ProfileHeaderView: UIView {
     
-    
     private lazy var imageView: UIImageView = {
         let morty = UIImage(named: "morty")
         let imageView = UIImageView(image: morty!)
         imageView.layer.cornerRadius = imageView.frame.size.width / 2
         imageView.clipsToBounds = true
-        
-//        imageView.layer.masksToBounds = true
-//        imageView.layer.shouldRasterize = true
-
-//        imageView.contentMode = UIView.ContentMode.scaleAspectFit
-//        imageView.layer.cornerRadius = 100
-//        imageView.clipsToBounds = true
-//        imageView.layer.borderWidth = 10
-//        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.layer.cornerRadius = 60
+        imageView.layer.borderWidth = 3
+        imageView.layer.borderColor = UIColor.black.cgColor
+        imageView.translatesAutoresizingMaskIntoConstraints = false
 
         return imageView
-        
     }()
     
     private lazy var title: UILabel = {
         let title = UILabel()
-        title.backgroundColor = .systemBlue
+        title.text = "Morty Smith"
+        title.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        title.translatesAutoresizingMaskIntoConstraints = false
+        
         return title
     }()
+    
+    private lazy var status: UILabel = {
+        let status = UILabel()
+        status.text = "Okaaaaaaay"
+        status.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        status.textColor = UIColor.gray
+        status.translatesAutoresizingMaskIntoConstraints = false
+        
+        return status
+    }()
+    
+    private lazy var statusButton: UIButton = {
+        let statusButton = UIButton(type: .system)
+        statusButton.backgroundColor = .systemBlue
+        statusButton.setTitleColor(.white, for: .normal)
+        statusButton.setTitle("Show status", for: .normal)
+        statusButton.addTarget(self, action: #selector(self.tap), for: .touchUpInside)
+        statusButton.layer.cornerRadius = 14
+        statusButton.layer.shadowRadius = 4
+        statusButton.layer.shadowOffset.width = 4
+        statusButton.layer.shadowOffset.height = 4
+        statusButton.layer.shadowColor = UIColor.black.cgColor
+        statusButton.layer.shadowOpacity = 0.7
+        statusButton.translatesAutoresizingMaskIntoConstraints = false
+
+        return statusButton
+    }()
+    
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
+        textField.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.backgroundColor = .white
+        textField.layer.cornerRadius = 12
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textField
+    }()
+    
+    private lazy var statusText: String = ""
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -44,13 +82,57 @@ class ProfileHeaderView: UIView {
         super.init(coder: coder)
     }
     
+    private func setupConstraints() {
+        
+        let safeArea = self.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
+            imageView.widthAnchor.constraint(equalToConstant: 125),
+            imageView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 16),
+            imageView.heightAnchor.constraint(equalToConstant: 125),
+            
+            title.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            title.widthAnchor.constraint(equalToConstant: 110),
+            title.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 27),
+            title.heightAnchor.constraint(equalToConstant: 18),
+            
+            status.widthAnchor.constraint(equalToConstant: 100),
+            status.heightAnchor.constraint(equalToConstant: 20),
+            status.bottomAnchor.constraint(equalTo: textField.topAnchor, constant: -16),
+            status.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            
+            statusButton.heightAnchor.constraint(equalToConstant: 50),
+            statusButton.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 185),
+            statusButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            statusButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            
+            textField.heightAnchor.constraint(equalToConstant: 40),
+            textField.bottomAnchor.constraint(equalTo: statusButton.topAnchor, constant: -16),
+            textField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            textField.leadingAnchor.constraint(equalTo: status.leadingAnchor)
+        ])
+    }
+    
     private func setupView() {
         self.addSubview(imageView)
         self.addSubview(title)
-        
-        self.imageView.frame = CGRect(x: 16, y: 116, width: 125, height: 125)
-//        self.title.frame = CGRect(x: 300, y: 200, width: 10, height: 10)
+        self.addSubview(status)
+        self.addSubview(statusButton)
+        self.addSubview(textField)
+        setupConstraints()
     }
     
-//    private func
+    @objc private func tap() {
+        let text = statusText
+        status.text = text
+    }
+    
+    @objc private func statusTextChanged() {
+        let input = textField.text
+        statusText = input!
+    }
+    
 }
+
+
--- Profile/ProfileViewController.swift
+++ Profile/ProfileViewController.swift
@@ -9,8 +9,6 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    
-    
     private lazy var profileHeaderView: ProfileHeaderView = {
        let view = ProfileHeaderView()
         return view
@@ -20,11 +18,18 @@ class ProfileViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = .lightGray
         self.navigationItem.title = "Profile"
-        
-        
         self.view.addSubview(self.profileHeaderView)
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(self.handleTap))
+        self.view.addGestureRecognizer(gesture)
     }
+    
     override func viewWillLayoutSubviews() {
-//        profileHeaderView.frame = view.bounds
+        profileHeaderView.frame = view.self.frame
     }
+    
+    @objc private func handleTap() {
+        self.view.endEditing(true)
+    }
+    
 }
