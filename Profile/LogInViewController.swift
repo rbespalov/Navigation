6340c47673b14c338f67d3ace075e8f461111293
Roman Bespalov <voordex@gmail.com>
2022-07-14 13:36:49 +0300

    add for loginVC button

--- Profile/LogInViewController.swift
+++ Profile/LogInViewController.swift
@@ -114,8 +114,7 @@ class LogInViewController: UIViewController {
         logInButton.layer.cornerRadius = 10
         logInButton.setTitleColor(.white, for: .normal)
         logInButton.setTitle("Log In", for: .normal)
-        let colorSet = UIColor(hex: "#4885CC")
-        logInButton.backgroundColor = colorSet
+        logInButton.backgroundColor = UIColor(named: "СolorForLogo")
         
         return logInButton
     }()
@@ -239,39 +238,5 @@ extension LogInViewController: UITextFieldDelegate {
         textField.resignFirstResponder()
         
         return true
-    }
-}
-
-extension UIColor {
-    
-    convenience init?(hex: String) {
-        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
-        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
-        
-        let lengh = hexSanitized.count
-        
-        var rgb: UInt64 = 0
-        
-        var r: CGFloat = 0.0
-        var g: CGFloat = 0.0
-        var b: CGFloat = 0.0
-        var a: CGFloat = 1.0
-        
-        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else {return nil}
-        
-        if lengh == 6 {
-            r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
-            g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
-            b = CGFloat(rgb & 0x0000FF) / 255.0
-        } else if lengh == 8 {
-            r = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
-            g = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
-            b = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
-            a = CGFloat(rgb & 0x000000FF) / 255.0
-        } else {
-            return nil
-        }
-        
-        self.init(red: r, green: g, blue: b, alpha: a)
     }
 }
